9d7cf11753e61384dbba2067f7a23805b2ce6f06
ianbaremans <32411363+ianbaremans@users.noreply.github.com>
2017-10-02 12:39:01 +0200

    Added function to change tags

--- pythonmp3.py
+++ pythonmp3.py
@@ -1,5 +1,6 @@
 # importing the necessary modules
 import stagger
+import sys
 
 # user inputs the path to the mp3
 path = input("Enter the path to your MP3: ")
@@ -10,11 +11,40 @@ print("Title: ", tag.title)
 print("Artist: ", tag.artist)
 print("Album: ", tag.album)
 print("Track: ", tag.track)
+print('To change a tag type tag:value (example: title:Champions)')
 
-# letting the user enter changes
-print('To change a tag type tag:"change" (example: title:"Champions")')
-print("If you're done enter exit")
-changes = input("Enter change: ")
 
-# writing the changes to the file
-tag.write()
+# function to change the tags
+def tagchange():
+    print("If you're done enter exit")
+    changes = input("Enter change: ")
+    if changes == "exit":
+        sys.exit()
+    else:
+        temp_changes = changes[:5]
+        if (temp_changes == "Title") or (temp_changes == "title"):
+            new_tag = changes[6:]
+            tag.title = new_tag
+            tag.write()
+            print("Title changed!")
+            tagchange()
+        elif (temp_changes == "Artis") or (temp_changes == "artis"):
+            new_tag = changes[7:]
+            tag.artist = new_tag
+            tag.write()
+            print("Artist changed!")
+            tagchange()
+        elif (temp_changes == "Album") or (temp_changes == "album"):
+            new_tag = changes[6:]
+            tag.album = new_tag
+            tag.write()
+            print("Album changed!")
+            tagchange()
+        elif (temp_changes == "Track") or (temp_changes == "track"):
+            new_tag = changes[6:]
+            tag.track = new_tag
+            tag.write()
+            print("Track changed!")
+            tagchange()
+
+tagchange()
